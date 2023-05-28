5bd5847a75e93ac5cef7ad61003c626995e9da90
eraymeric2016 <125488259+eraymeric2016@users.noreply.github.com>
2023-05-28 14:27:08 +0300

    Add files via upload

---
+++ PreincrementDecrement.java
@@ -0,0 +1,44 @@
+package week_04;
+
+public class PreincrementDecrement {
+    public static void main(String[] args) {
+
+        int a=10;
+
+        int b=++a;
+
+        System.out.println(b);
+
+        System.out.println("--------------------");
+
+
+        int c=--b;
+
+
+        System.out.println(b--);
+        System.out.println("--------------------");
+        System.out.println("b=" + b);
+        System.out.println("--------------------");
+        System.out.println(c);
+        System.out.println("--------------------");
+        System.out.println(a++ + b++ + c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
